a74fef3af8eb28a322efd3cce71c1bf63eba4d9c
Sriteja Kattamuru <131655915+sriteja28@users.noreply.github.com>
2023-10-03 14:11:43 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 module "vpc" {
   source = "git::https://github.com/sriteja28/tf-module-vpc.git"
 
-  for_each = var.vpc
+  for_each   = var.vpc
   cidr_block = each.value["cidr_block"]
-  subnets = each.value["subnets"]
+  subnets    = each.value["subnets"]
 
-  env = var.env
-  tags = var.tags
+  env            = var.env
+  tags           = var.tags
   default_vpc_id = var.default_vpc_id
   default_vpc_rt = var.default_vpc_rt
 
@@ -15,21 +15,24 @@ module "vpc" {
 module "app_server" {
   source = "git::https://github.com/sriteja28/tf-module-app.git"
 
-  env = var.env
-  tags = var.tags
+  env       = var.env
+  tags      = var.tags
   component = "test"
-  subnet_id = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), "app", null),"subnet_ids", null)[0]
-  vpc_id = lookup(lookup(module.vpc, "main",null), "vpc_id", null)
+  subnet_id = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), "app", null), "subnet_ids", null)[0]
+  vpc_id    = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
 }
 
 module "rabbitmq" {
   source = "git::https://github.com/sriteja28/tf-module-rabbitmq.git"
-  for_each = var.rabbitmq
-  component = each.value["component"]
-  instance_type = each.value["instance_type"]
-  sg_subnet_cidr = lookup(lookup(lookup(lookup(var.vpc,"main", null), "subnets", null), "app", null), "cidr_block", null)
-  vpc_id = lookup(lookup(module.vpc, "main",null), "vpc_id", null)
 
-  env = var.env
+  for_each      = var.rabbitmq
+  component     = each.value["component"]
+  instance_type = each.value["instance_type"]
+
+  sg_subnet_cidr = lookup(lookup(lookup(lookup(var.vpc, "main", null), "subnets", null), "app", null), "cidr_block", null)
+  vpc_id         = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
+  subnet_id      = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), "db", null), "subnet_ids", null)[0]
+
+  env  = var.env
   tags = var.tags
 }
