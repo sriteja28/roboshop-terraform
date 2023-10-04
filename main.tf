7c762596eaa427e7ae3b6a30dd299ce98db4adf6
Sriteja Kattamuru <131655915+sriteja28@users.noreply.github.com>
2023-10-04 19:03:06 +0530

    Updating Terraform files for the project

--- main.tf
+++ main.tf
@@ -100,38 +100,38 @@ module "vpc" {
 #  kms_key_arn = var.kms_key_arn
 #}
 
-module "alb" {
-  source = "git::https://github.com/sriteja28/tf-module-alb.git"
-
-  for_each           = var.alb
-  name               = each.value["name"]
-  internal           = each.value["internal"]
-  load_balancer_type = each.value["load_balancer_type"]
-  vpc_id             = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
-  sg_subnet_cidr     = each.value["name"] == "public" ? ["0.0.0.0/0"] : local.app_web_subnet_cidr
-  subnets            = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), each.value["subnet_ref"], null), "subnet_ids", null)
-
-  env  = var.env
-  tags = var.tags
-}
-
-
-module "app_server" {
-  source = "git::https://github.com/sriteja28/tf-module-app.git"
-
-  for_each         = var.apps
-  app_port         = each.value["app_port"]
-  desired_capacity = each.value["desired_capacity"]
-  max_size         = each.value["max_size"]
-  min_size         = each.value["min_size"]
-  instance_type    = each.value["instance_type"]
-  sg_subnets_cidr  = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), each.value["subnet_ref"], null), "subnet_ids", null)
-
-  component = each.value["component"]
-  subnets   = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), each.value["subnet_ref"], null), "subnet_ids", null)
-  vpc_id    = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
-
-  env         = var.env
-  tags        = var.tags
-  kms_key_arn = var.kms_key_arn
-}
+#module "alb" {
+#  source = "git::https://github.com/sriteja28/tf-module-alb.git"
+#
+#  for_each           = var.alb
+#  name               = each.value["name"]
+#  internal           = each.value["internal"]
+#  load_balancer_type = each.value["load_balancer_type"]
+#  vpc_id             = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
+#  sg_subnet_cidr     = each.value["name"] == "public" ? ["0.0.0.0/0"] : local.app_web_subnet_cidr
+#  subnets            = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), each.value["subnet_ref"], null), "subnet_ids", null)
+#
+#  env  = var.env
+#  tags = var.tags
+#}
+#
+#
+#module "app_server" {
+#  source = "git::https://github.com/sriteja28/tf-module-app.git"
+#
+#  for_each         = var.apps
+#  app_port         = each.value["app_port"]
+#  desired_capacity = each.value["desired_capacity"]
+#  max_size         = each.value["max_size"]
+#  min_size         = each.value["min_size"]
+#  instance_type    = each.value["instance_type"]
+#  sg_subnets_cidr  = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), each.value["subnet_ref"], null), "subnet_ids", null)
+#
+#  component = each.value["component"]
+#  subnets   = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), each.value["subnet_ref"], null), "subnet_ids", null)
+#  vpc_id    = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
+#
+#  env         = var.env
+#  tags        = var.tags
+#  kms_key_arn = var.kms_key_arn
+#}
