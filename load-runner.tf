6855870afae7641a3498815073d73158a7a14069
Sriteja Kattamuru <131655915+sriteja28@users.noreply.github.com>
2023-10-09 13:55:17 +0530

    Updating Terraform files for the project

---
+++ load-runner.tf
@@ -0,0 +1,27 @@
+data "aws_ami" "ami" {
+  owners      = ["973714476881"]
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+}
+
+
+resource "aws_instance" "load-runner" {
+  ami                    = data.aws_ami.ami.id
+  instance_type          = "t3.medium"
+  vpc_security_group_ids = ["sg-0df2cde9ab8f70430"]
+  tags = {
+    Name = "load-runner"
+  }
+
+  provisioner "remote-exec" {
+    connection {
+      user     = "root"
+      password = "DevOps321"
+      host     = self.public_ip
+    }
+    inline = [
+      "curl https://raw.githubusercontent.com/linuxautomations/labautomation/master/tools/docker/install.sh | bash",
+      "docker pull robotshop/rs-load:latest"
+    ]
+  }
+}
